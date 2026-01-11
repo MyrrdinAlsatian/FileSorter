c346354942bc49b9115dbf432623b071d9b093e6
Jean-Ba <stephan.jeanba@gmail.com>
2026-01-11 01:06:09 +0100

    refactor: :sparkles: reorganize Result and StatsFile types for improved clarity and structure

--- types/types.go
+++ types/types.go
@@ -7,3 +7,17 @@ type Stats struct {
 
 	DetectedFileType map[string]int
 }
+
+type Result struct {
+	Path   string
+	Size   int64
+	Type   string
+	Method string
+	Error  string
+}
+
+type StatsFile struct {
+	FilesByType map[string]int64
+	BytesByType map[string]int64
+	Errors      int64
+}
