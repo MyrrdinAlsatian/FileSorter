b11dcce7754cc6a44332de437aa1d09587bd295a
Jean-Ba <stephan.jeanba@gmail.com>
2026-01-23 19:53:39 +0100

    feat: reorder fields in FileData struct for improved readability

--- metadata/types.go
+++ metadata/types.go
@@ -3,7 +3,8 @@ package metadata
 import "time"
 
 type FileData struct {
-	Time   time.Time
-	Source string // Source of the metadata (e.g., "filesystem", "exif", etc.)
-	Valid  bool
+	OriginalName string
+	Time         time.Time
+	Source       string // Source of the metadata (e.g., "filesystem", "exif", etc.)
+	Valid        bool
 }
